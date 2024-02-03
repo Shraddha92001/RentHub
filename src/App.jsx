b93d5651a80da99e2b4ef143df7cff5628a12fa1
shraddha-singh <shraddhasingh92001@gmail.com>
2024-02-03 22:41:49 +0530

    create first template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/signUp';
+import SignIn from './pages/signIn';
+import About from './pages/about';
+import Profile from './pages/Profile';
 
 export default function App() {
-    return <h1 className='text-red-500'> my App </h1>;
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    </BrowserRouter>
 }
